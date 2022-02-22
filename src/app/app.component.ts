661d240e538e0cb6405e0c5844ecb60c04c18050
Burak Goksel <burak.goksel@prospeum.de>
2022-02-22 13:04:58 +0100

    add editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component, VERSION } from '@angular/core';
+import {
+  ToolbarSettingsModel,
+  ToolbarType,
+  PasteCleanupSettingsModel,
+  HtmlEditorService,
+  ImageService,
+  LinkService,
+  ToolbarService,
+} from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  /**
+   * Sync Fusion html editor config
+   */
+  public tools: ToolbarSettingsModel = {
+    items: [
+      'Bold',
+      'Italic',
+      'Underline',
+      'StrikeThrough',
+      'SubScript',
+      'SuperScript',
+      '|',
+      'Formats',
+      'FontName',
+      'FontSize',
+      'FontColor',
+      'BackgroundColor',
+      '|',
+      'Alignments',
+      'OrderedList',
+      'UnorderedList',
+      'Indent',
+      'Outdent',
+      '|',
+      'Image',
+      'CreateLink',
+      '|',
+      'Undo',
+      'Redo',
+    ],
+  };
+  public pasteCleanupSettings: PasteCleanupSettingsModel = {
+    prompt: true,
+    plainText: false,
+    keepFormat: false,
+  };
+
+  public value = 'Hi';
 }
